49a3ef5cb580de111b66102ca557b3fe8ee45e77
Simeon Kakpovi <skakpovi@gmail.com>
2015-08-21 01:42:02 +0000

    D

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -9,13 +9,16 @@ mask:'9999/19/39 29:59'
 });
 
 
-$('.some_class').datetimepicker();
 
-$('#default_datetimepicker').datetimepicker({
-	formatTime:'H:i',
-	formatDate:'d.m.Y',
-	//defaultDate:'8.12.1986', // it's my birthday
-	defaultDate:'+03.01.1970', // it's my birthday
-	defaultTime:'10:00',
-	timepickerScrollbar:false
-});
+
+
+  $('#calendar').fullCalendar({
+    events: '/bookings.json',
+    eventLimit: true, 
+    header: {
+				left: 'prev,next today',
+				center: 'title',s
+				right: 'month,agendaWeek,agendaDay'
+			}
+  });
+
