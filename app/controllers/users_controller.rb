ee097a4111214efa61bb53f82b493f9902074745
Simeon Kakpovi <skakpovi@gmail.com>
2015-08-31 01:14:38 +0000

    homep page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :manage]
     
   def show
-    @spaces = Space.where(user_id: current_user.id)
+    @spaces = Space.where(user_id: @user.id)
     respond_to do |format|
         format.html # show.html.erb
         format.xml { render :xml => @user }
