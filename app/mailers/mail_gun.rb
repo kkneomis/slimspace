3f5d091ac8e925ec94f62dd9c6b1a98982a1a907
Simeon Kakpovi <skakpovi@gmail.com>
2015-09-17 00:11:26 +0000

    mailer

--- app/mailers/mail_gun.rb
+++ app/mailers/mail_gun.rb
@@ -3,9 +3,9 @@ class MailGun < ApplicationMailer
   def sign_up_success(email)
       mail to: email, subject:"Welcome to SlimSpace.io"
   end
-  
+
   def approve_deny(space,booking,user,email)
-    mail to: email, subject:"You have a new request"  
+    mail to: email, subject:"You have a new request"
   end
-  
+
 end
