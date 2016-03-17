6c1fd36132e36da5166fd7a6d41efeab349cc43c
Simeon Kakpovi <skakpovi@gmail.com>
2016-03-17 16:51:25 -0400

    rm .dea

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,21 @@
 // require underscore
 // require gmaps/google
 //= require_tree .
+
+
+$(document).ready(function() {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "positionClass": "toast-bottom-right",
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+});
