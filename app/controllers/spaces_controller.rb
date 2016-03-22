fe2ec64b63f42e089584daa492cfc882f012428e
Simeon Kakpovi <skakpovi@gmail.com>
2016-03-21 23:29:42 -0400

    addressed bug

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -4,11 +4,11 @@ class SpacesController < ApplicationController
   before_filter :check_user, only: [:edit, :update, :destroy]
   before_filter :check_privacy, only: [:show]
   #privacy
-  
-  
-  
+
+
+
   def show
-    @space = Space.find(params[:id])  
+    @space = Space.find(params[:id])
      @owner = User.find(@space.user_id)
     if @owner != current_user
       @desks = @space.desks.where(private: false)
@@ -18,14 +18,13 @@ class SpacesController < ApplicationController
   end
 
 
-
-  def index 
-    @spaces = Space.where(parent_id: nil).order('created_at DESC') 
-    if params[:searchDateRange] 
-      range = params[:searchDateRange].split('-')  
-    else  
-      range = nil
-    end 
+  def index
+    @spaces = Space.all
+    #if params[:searchDateRange]
+    #  range = params[:searchDateRange].split('-')
+    #else
+    #  range = nil
+    #end
   end
 
   def new
@@ -36,8 +35,8 @@ class SpacesController < ApplicationController
   def create
     @space = Space.new(space_params)
     @space.user_id = current_user.id
-    
-   respond_to do |format| 
+
+   respond_to do |format|
     if @space.save
       format.html { redirect_to @space, notice: 'Space was successfully updated.' }
       format.json { render :show, status: :ok, location: @space }
@@ -74,22 +73,22 @@ class SpacesController < ApplicationController
   def set_space
     @space = Space.find(params[:id])
     end
-  
+
 
     def space_params
       params.require(:space).permit(:name, :address, :description, :city, :state, :zip, :price, :number_of_seats, :user_id, :image, :parent_id, :level, :can_book, :private, :scale, :confirm)
     end
-  
+
       def check_user
         if current_user.id != @space.user_id
           redirect_to root_url, alert: "Sorry, this Space belongs to someone else"\
         end
       end
-  
+
       def check_privacy
         if (@space.private == true) && ((!user_signed_in?) || (current_user.id != @space.user_id))
           redirect_to root_url, alert: "Sorry, this Space is private"\
         end
       end
-  
+
 end
