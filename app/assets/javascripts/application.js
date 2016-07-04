34c9acae82173de14b71e5ee136fb00bec697a0b
Simeon Kakpovi <skakpovi@gmail.com>
2016-07-04 18:14:27 -0400

    fixed address problem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,21 +24,21 @@
 //= require_tree .
 
 $(document).ready(function() {
-    toastr.options = {
-      "closeButton": true,
-      "debug": true,
-      "newestOnTop": true,
-      "progressBar": true,
-      "positionClass": "toast-top-right",
-      "preventDuplicates": false,
-      "showDuration": "10000",
-      "hideDuration": "1000",
-      "timeOut": "5000",
-      "extendedTimeOut": "1000",
-      "showEasing": "swing",
-      "hideEasing": "linear",
-      "showMethod": "fadeIn",
-      "hideMethod": "fadeOut"
-    }
-	
+toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": true,
+  "progressBar": true,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "10000",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
 	});
